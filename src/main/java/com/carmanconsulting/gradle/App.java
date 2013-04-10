6898c5a6d93c84871fed9ab229c36bc9adf815b5
James Carman <james@carmanconsulting.com>
2013-04-10 16:23:51 -0400

    Initial

---
+++ src/main/java/com/carmanconsulting/gradle/App.java
@@ -0,0 +1,13 @@
+package com.carmanconsulting.gradle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
